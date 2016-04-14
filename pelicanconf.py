4f86a529bd494a5785ff719092c74bf766be62df
Federico Vaggi <vaggi.federico@gmail.com>
2016-04-14 18:52:38 +0200

    Removed links and added ipynb plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,16 +20,17 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = 10
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = './plugins'
+PLUGINS = ['ipynb.markup']
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
