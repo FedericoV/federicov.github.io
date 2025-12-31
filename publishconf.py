c1ffba42b8227f9569881bf86a1dd58a1c5d86bc
Federico Vaggi <vaggi.federico@gmail.com>
2025-12-31 10:59:28 -0800

    Final css tweaks and remove atom

--- publishconf.py
+++ publishconf.py
@@ -9,7 +9,10 @@ from pelicanconf import *
 SITEURL = 'https://federicov.github.io'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 DELETE_OUTPUT_DIRECTORY = True
